9f60b07fa47bdc2ec0896b7778dec1f2560fc351
SailorStarHealer <108467609+SailorStarHealer@users.noreply.github.com>
2022-07-14 14:03:38 +0200

    Lists and Loops
    Stuff for school

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var summed = new List<int> { };
+for (int i = 0; i < 7; i++)
+{
+    var random = new Random();
+    int roll1 = random.Next(1, 7);
+    int roll2 = random.Next(1, 7);
+    int roll3 = random.Next(1, 7);
+    int roll4 = random.Next(1, 7);
+    Console.Write($"You roll {roll1}, {roll2}, {roll3} {roll4}. ");
+    var rolls = new List<int> { roll1, roll2, roll3, roll4 };
+    rolls.Sort();
+    foreach (var roll in rolls) ;
+    rolls.Remove(rolls[0]);
+
+    Console.WriteLine($"The ability score is {rolls[0] + rolls[1] + rolls[2]}");
+    int sum = rolls[0] + rolls[1] + rolls[2];
+summed.Add(sum);
+}
+
+Console.WriteLine($"Your available ability scores are");
+foreach (var sum in summed) {
+    Console.Write($"{sum}, "); }
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//WorkWithString();
+
+  //  void WorkWithString()
+//{
+  //  var names = new List<string> { "Yuka>", "Anna", "Felipe" };
+    //foreach (var name in names)
+   // {
+    //    Console.WriteLine($"Hello{name.ToUpper()}!");
+   // }
+
+  //  Console.WriteLine();
+  //  names.Add("Maria");
+  //  names.Add("Bill");
+  //  names.Remove("Anna");
+  //  foreach (var name in names)
+  //  {
+ //       Console.WriteLine($"Hello{name.ToUpper()}!");
+  //  }
+
+ //   Console.WriteLine($"My name is {names[0]}");
+//    Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+  //  Console.WriteLine($"The list has {names.Count} people in it");
+
+  //  var index = names.IndexOf("Felipe");
+  //  if (index == -1)
+  //  {
+  //      Console.WriteLine($"When an item is not found, IndexOf returns{index}");
+ //   }
+ //   else
+ //   {
+  //      Console.WriteLine($"The name {names[index]} is at index {index}");
+//    }
+  //  index = names.IndexOf("Not Found");
+//    if (index == -1)
+  //  {
+    //    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+  //  }
+  //  else
+//    {
+    //    Console.WriteLine($"The name {names[index]} is at index {index}");
+//    }
+  //  names.Sort();
+   // foreach (var name in names)
+    //{
+//        Console.WriteLine($"Hello {name.ToUpper()}!");
+  //  }
+//}
+
+List <int> fibonacciNumbers = new List<int> { 1, 1 };
+
+for (int index = 0; index < 18; index++)
+{
+    int previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    int previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+    fibonacciNumbers.Add(previous + previous2);
+}
+foreach (var item in fibonacciNumbers)
+    Console.WriteLine(item);
